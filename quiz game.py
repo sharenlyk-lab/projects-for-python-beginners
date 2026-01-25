4883e24368f7a467e2d7135a38698927ee9d5318
sharenlyk-lab <sharen.lyk@gmail.com>
2026-01-25 21:41:52 +0800

    Add files via upload

---
+++ quiz game.py
@@ -0,0 +1,25 @@
+print("Welcome to my Computer Quiz".title())
+
+playing = input ("Do you want to play?    ")
+
+print (playing)
+
+if playing.lower() != "yes": 
+    quit()
+
+print("Okay let's play :)")
+
+answer=input("What does CPU stand for?  ")
+
+if answer.lower()== "central processing unit":
+    print("CORRECT")
+else:
+    print("INCORRECT")
+
+answer= imput= ("What does GPU stand for?   ")
+
+if answer.lower()== "graphics processing unit":
+    print("Correct")
+else: 
+    print("Incorrect") 
+    
